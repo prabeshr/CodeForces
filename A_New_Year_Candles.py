412e45611e15ca65590930a6cfe54f8df19c4b6f
Prabesh Regmi <naturel.rp@gmail.com>
2024-12-26 18:25:53 +0545

    Testing

--- A_New_Year_Candles.py
+++ A_New_Year_Candles.py
@@ -1,13 +1,6 @@
 def solve():
     numbers = list(map(int, input().split()))
     
-
-# Range: 'a' to 'z'
-# ASCII Values: 97 to 122
-
-# Range: 'A' to 'Z'
-# ASCII Values: 65 to 90
-
 a = int(input())
 for _ in range(a):
     solve()
